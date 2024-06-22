ad527d59b67ff08a72f41af645b6263e25880832
Daniel Mössner <github@moesibert.de>
2024-06-22 16:57:29 +0200

    update certificates

---
+++ src/content/_certificates.js
@@ -0,0 +1,22 @@
+export const CERTIFICATES = [
+    {
+        name: "PyCon 24",
+        date: "2024-04-02",
+        pdf: "/certificates/pycon.pdf"
+    },
+    {
+        name: "The Anatomy of a Deployment Pipeline",
+        date: "2023-04-01",
+        pdf: "/certificates/deployment-pipelines.pdf"
+    },
+    {
+        name: "CD - Better Software Faster",
+        date: "2023-03-21",
+        pdf: "/certificates/deployment-pipelines.pdf"
+    },
+    {
+        name: "Bachelor of Science - Informatik",
+        date: "2022-07-22",
+        pdf: "/certificates/uni.pdf"
+    },
+]
