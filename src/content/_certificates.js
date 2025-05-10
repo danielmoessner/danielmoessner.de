c1cc902b85216d4f380782718cc7964d101d6344
danielmoessner <github@danielmoessner.de>
2025-05-10 17:48:34 +0200

    update

--- src/content/_certificates.js
+++ src/content/_certificates.js
@@ -1,27 +1,32 @@
 export const CERTIFICATES = [
-    {
-        name: "Introduction to Electricity and Magnetism",
-        date: "2024-07-01",
-        pdf: "/certificates/introduction-to-electricity-and-magnetism.pdf",
-    },
-    {
-        name: "PyCon 24",
-        date: "2024-04-02",
-        pdf: "/certificates/pycon.pdf"
-    },
-    {
-        name: "The Anatomy of a Deployment Pipeline",
-        date: "2023-04-01",
-        pdf: "/certificates/deployment-pipelines.pdf"
-    },
-    {
-        name: "CD - Better Software Faster",
-        date: "2023-03-21",
-        pdf: "/certificates/cd-better-software.pdf"
-    },
-    {
-        name: "Bachelor of Science - Informatik",
-        date: "2022-07-22",
-        pdf: "/certificates/uni.pdf"
-    },
-]
+  {
+    name: "RxJS Masterclass",
+    date: "2025-05-10",
+    pdf: "/certificates/rxjs-masterclass.pdf",
+  },
+  {
+    name: "Introduction to Electricity and Magnetism",
+    date: "2024-07-01",
+    pdf: "/certificates/introduction-to-electricity-and-magnetism.pdf",
+  },
+  {
+    name: "PyCon 24",
+    date: "2024-04-02",
+    pdf: "/certificates/pycon.pdf",
+  },
+  {
+    name: "The Anatomy of a Deployment Pipeline",
+    date: "2023-04-01",
+    pdf: "/certificates/deployment-pipelines.pdf",
+  },
+  {
+    name: "CD - Better Software Faster",
+    date: "2023-03-21",
+    pdf: "/certificates/cd-better-software.pdf",
+  },
+  {
+    name: "Bachelor of Science - Informatik",
+    date: "2022-07-22",
+    pdf: "/certificates/uni.pdf",
+  },
+];
